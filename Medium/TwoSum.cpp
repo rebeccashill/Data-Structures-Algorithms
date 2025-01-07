a1af6c0aa598c83cff0baa7deb0f98b937e09b52
Rebecca Shillingford <51681289+rebeccashill@users.noreply.github.com>
2025-01-07 14:02:53 -0500

    Update TwoSum.cpp
    Clean up code in IDE.

--- Medium/TwoSum.cpp
+++ Medium/TwoSum.cpp
@@ -1,33 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+using namespace std;
+
 //Sorting + 2 pointer solution
 //Runtime: 8 ms, faster than 99.93% of C++ online submissions for Two Sum.
 //Memory Usage: 9.5 MB, less than 70.01% of C++ online submissions for Two Sum.
 //Time: O(NlogN), space: O(N)
+
 class Solution {
 public:
     template <typename T>
     vector<int> sort_indexes(const vector<T> &v) {
 
       // initialize original index locations
-      vector<int> idx(v.size());
-      iota(idx.begin(), idx.end(), 0);
+      vector<int> index(v.size());
+      iota(index.begin(), index.end(), 0);
 
       // sort indexes based on comparing values in v
-      sort(idx.begin(), idx.end(),
+      sort(index.begin(), index.end(),
            [&v](int i1, int i2) {return v[i1] < v[i2];});
 
-      return idx;
+      return index;
     }
-    
+
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ixs = sort_indexes(nums);
-        
+        vector<int> indexes = sort_indexes(nums);
+
         int i = 0, j = nums.size()-1;
-        
+
         while(i < j){
-            int ix1 = ixs[i], ix2 = ixs[j];
-            if(nums[ix1] + nums[ix2] == target){
-                return vector<int> {ix1, ix2};
-            }else if(nums[ix1] + nums[ix2] < target){
+            int index1 = indexes[i], index2 = indexes[j];
+            if(nums[index1] + nums[index2] == target){
+                return vector<int> {index1, index2};
+            }else if(nums[index1] + nums[index2] < target){
                 i++;
             }else{
                 j--;
@@ -36,3 +43,8 @@ public:
         return vector<int>();
     }
 };
+
+int main(void) {
+ Solution solut();
+ return 0;
+}
