138b85cbcc66237e338d436318d68e175404f5d2
Rebecca Shillingford <51681289+rebeccashill@users.noreply.github.com>
2025-02-02 17:12:14 -0500

    Add rotateRight
    Manipulate ListNodes in order to rotate a list to the right.

--- Medium/Rotate List.cpp
+++ Medium/Rotate List.cpp
@@ -1,29 +1,49 @@
-/* Slower solution:
-int findway(int* nums, int sum, int target, int present, int numsSize) {
-    if (present == numsSize) {
-        return (sum == target) ? 1 : 0;
-    }
-
-    // Recurse with adding the current number
-    int sum1 = sum + nums[present];
-    int sum2 = sum - nums[present];
-    
-    // Move to the next index
-    return findway(nums, sum1, target, present + 1, numsSize) + 
-           findway(nums, sum2, target, present + 1, numsSize);
-}
-
-int findTargetSumWays(int* nums, int numsSize, int target) {
-    return findway(nums, 0, target, 0, numsSize);
-} */
-
-
- // Solution 1.
+//Solution that uses more memory:
+// Solution 1.
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !k) return head;
+        ListNode* tail(head), *cur(head), *res;
+        
+        int len = 1;
+        while(tail->next) tail = tail->next, len++;
+        
+        k = k % len;
+        if(!k) return head;
+        k = len - k;
+        
+        while(--k) cur = cur->next;
+        
+        res = cur->next;
+        cur->next = NULL;
+        tail->next = head;
+        
+        return res;
+    }
+};
+
+
+ // Solution 2.
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        // If the list does not exist or there's no listnode next, return the listnode
         if(!head || !head->next) return head;
 
+        /* 1. Create a pointer ListNode P
+        *  2. Set ListNode* p = the first ListNode
+        *  3. Loop through the temporary list
+        *       If the length is 0, return the first ListNode
+        *  4. Create slow and fast pointers
+        *  5. Initiate the movement of the fast pointer
+        *       Decrement k (which is set to the length of the list)
+        *       Slow is 1 listnode behind
+        *  6. Keep incrementing fast and slow until we reach the end of fast
+        *       This will switch everything around in the p list.
+        *  7. Set res = slow.next which is the second to last ListNode so it is effectively rotated
+        *  8. Reset the list to begin at the start and end after the slow pointer (because everything is switched)
+        */
         int len = 0;
         ListNode* p = head;
         while(p) p = p->next, len++;
