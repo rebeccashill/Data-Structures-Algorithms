7047ba9df5b9e643e1e3723487b87888537fc294
Rebecca Shillingford <51681289+rebeccashill@users.noreply.github.com>
2025-02-23 15:53:26 -0500

    Update PermutationSequence.cpp
    Add spaces to clean up code.

--- Hard/PermutationSequence.cpp
+++ Hard/PermutationSequence.cpp
@@ -9,21 +9,21 @@ public:
         int numLoops = 0;
         int fact = 1;
         vector<int> nums;
-        for(int i = 1;i < numLoops;i++){
+        for(int i = 1; i < numLoops; i++){
             fact = fact * i;
             nums.push_back(i);
         }
         nums.push_back(numLoops);
-        k=k-1;
-        string ans="";
+        k = k-1;
+        string ans = "";
         while(true){
-            ans=ans+to_string(nums[k/fact]);
-            nums.erase(nums.begin()+k/fact);
-            if(nums.size()==0){
+            ans = ans + to_string(nums[k/fact]);
+            nums.erase(nums.begin()+k / fact);
+            if (nums.size() == 0) {
                 break;
             }
-            k=k%fact;
-            fact=fact/nums.size();
+            k = k % fact;
+            fact = fact / nums.size();
         }
         return ans;
     }
