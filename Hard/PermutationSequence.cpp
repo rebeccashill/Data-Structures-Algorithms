2726d71bf91a8a8862ef466443dd31aa4e426a89
Rebecca Shillingford <51681289+rebeccashill@users.noreply.github.com>
2025-02-23 17:20:33 -0500

    Update PermutationSequence.cpp
    Add comments to PermutationSequence.

--- Hard/PermutationSequence.cpp
+++ Hard/PermutationSequence.cpp
@@ -3,6 +3,19 @@
 
 using namespace std;
 
+/* getPermutation (Leetcode Solution)
+    Parameters: int n (amount of numbers in the sequence), int k (the kth permutation in the sequence)
+    Method: dynamic programming
+    1. Initialize variables for numLoops, factorial, and vector of numbers.
+    2. Loop through the permutation.
+        - Find the next factorial given i.
+        - Add i to the end of the vector.
+    4. Add numLoops to the end of the vector.
+    6. Create the answer string.
+    5. Construct the answer whilst erasing values from the nums vector.
+        - When the nums vector has nothing in it, break out of the loop.
+    6. Return the answer string.
+*/
 class Solution {
 public:
     string getPermutation(int n, int k) {
